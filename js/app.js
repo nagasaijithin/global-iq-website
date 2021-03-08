a6e4d7854cde32f24e6ddfd8ec6767c99789ce7d
nagasaijithin <nagasaijithin1@gmail.com>
2021-03-09 02:09:32 +0530

    compilite the home

--- js/app.js
+++ js/app.js
@@ -1,32 +1,41 @@
-import "../scss/style.scss";
+import "../scss/app.scss";
 console.log("hello");
-const btn = document.querySelector(".btn2");
+// const btn = document.querySelector(".btn2");
 const db = firebase.firestore();
-btn.addEventListener("click", (e) => {
-  // db.collection("students")
-  //   .doc()
-  //   .set({
-  //     name: "naga saijithin 2",
-  //     state: "AP",
-  //     country: "IND",
-  //   })
-  //   .then(() => {
-  //     console.log("Document successfully written!");
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error writing document: ", error);
-  //   });
-});
+// btn.addEventListener("click", (e) => {
+//   // db.collection("students")
+//   //   .doc()
+//   //   .set({
+//   //     name: "naga saijithin 2",
+//   //     state: "AP",
+//   //     country: "IND",
+//   //   })
+//   //   .then(() => {
+//   //     console.log("Document successfully written!");
+//   //   })
+//   //   .catch((error) => {
+//   //     console.error("Error writing document: ", error);
+//   //   });
+// });
 
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    var uid = user.uid;
-    console.log(uid);
-    // ...
-  } else {
-    // User is signed out
-    // ...
+// firebase.auth().onAuthStateChanged((user) => {
+//   if (user) {
+//     // User is signed in, see docs for a list of available properties
+//     // https://firebase.google.com/docs/reference/js/firebase.User
+//     var uid = user.uid;
+//     console.log(uid);
+//     // ...
+//   } else {
+//     // User is signed out
+//     // ...
+//   }
+// });
+
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
   }
-});
+}, 5000);
